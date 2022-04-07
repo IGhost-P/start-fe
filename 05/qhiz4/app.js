4c9ca0370868f5159475d1d0f34bb406a6abbf14
IGhos-P <dndb3599@gmail.com>
2022-04-07 14:59:34 +0900

    drog and drop 변경

--- 05/qhiz4/app.js
+++ 05/qhiz4/app.js
@@ -1,42 +1,44 @@
-function App() {
-  $container = document.querySelector(".box-container");
-  $box = $container.querySelector(".box");
-  const { width: containerWidth, height: containerHeigtht } =
-    $container.getBoundingClientRect();
-  const { width: boxWidth, height: boxHeight } = $box.getBoundingClientRect();
-  let isDragging = null;
-  let originLeft = null;
-  let originTop = null;
-  let originX = null;
-  let originY = null;
+function drag() {
+  let dragging = false;
 
-  $box.addEventListener("mousedown", (e) => {
-    isDragging = true;
-    originX = e.clientX;
-    originY = e.clientY;
-    originLeft = $box.offsetLeft;
-    originTop = $box.offsetTop;
-  });
+  let mouseX, mouseY;
+  let eleX, eleY;
 
-  document.addEventListener("mouseup", (e) => {
-    isDragging = false;
-  });
+  let box = document.querySelector(".box");
+  box.addEventListener("mousedown", mouseDown);
+  box.style.top = 0;
+  box.style.left = 0;
 
-  document.addEventListener("mouseover", (e) => {
-    if (isDragging) {
-      const diffX = e.clientX - originX;
-      const diffY = e.clientY - originY;
-      const endOfXPoint = containerWidth - boxWidth;
-      const endOfYPoint = containerHeigtht - boxHeight;
-      $box.style.left = `${Math.min(
-        Math.max(0, originLeft + diffX),
-        endOfXPoint
-      )}px`;
-      $box.style.top = `${Math.min(
-        Math.max(0, originTop + diffY),
-        endOfYPoint
-      )}px`;
+  function mouseDown(e) {
+    e.preventDefault();
+
+    dragging = this;
+
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    eleX = Number.parseInt(dragging.style.left);
+    eleY = Number.parseInt(dragging.style.top);
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+  }
+
+  function mouseMove(e) {
+    if (dragging) {
+      deltaMouseX = e.pageX - mouseX;
+      deltaMouseY = e.pageY - mouseY;
+
+      dragging.style.left = deltaMouseX + eleX + "px";
+      dragging.style.top = deltaMouseY + eleY + "px";
     }
-  });
+  }
+
+  function mouseUp(e) {
+    dragging = false;
+
+    document.removeEventListener("mouseup", mouseUp);
+    document.removeEventListener("mousemove", mouseMove);
+  }
 }
-App();
+
+drag();
