3738a62ea0558d0de9b3b4bff9d246d7aa731bb8
IGhos-P <dndb3599@gmail.com>
2022-05-26 17:10:52 +0900

    bug : 경로 설정 완료

--- 12/quiz12/components/index.js
+++ 12/quiz12/components/index.js
@@ -1,6 +1,5 @@
 export { InputForm } from "./InputForm/index.js";
 export { Result } from "./Result/index.js";
-export { MoreBtn } from "./MoreBtn/index.js";
 export { AvgContainer } from "./AvgContainer/index.js";
 export { RanDomContainer } from "./RandomContainer/index.js";
 export { TwoInputForm } from "./TwoInputForm/index.js";
